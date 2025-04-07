d2945b7f79af6b34485f5cad24dbfc891947461b
Tubagus <tebe.aulia@gmail.com>
2025-04-07 23:32:34 +0700

    menambahkan class DataMahasiswa103022300141

---
+++ DataMahasiswa103022300141.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_4
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    class DataMahasiswa_103022300141
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public class JsonHelper
+    {
+        public static T ReadJson<T>(string filePath)
+        {
+            string jsonFile = File.ReadAllText(filePath);
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            T result = JsonSerializer.Deserialize<T>(jsonFile, options);
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace tpmodul7_kelompok_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "tp7_1_103022300141.json";
+            var data = JsonHelper.ReadJson<DataMahasiswa_103022300141>(filePath);
+
+            Console.WriteLine($"Nama {data.nama.depan} {data.nama.belakang} dengan nim {data.nim} dari fakultas {data.fakultas}");
+        }
+    }
+}
