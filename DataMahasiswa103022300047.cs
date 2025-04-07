d39658aac56447465b8b2fe10c4f87c16c7ad12f
Razky666 <razkypeace@gmail.com>
2025-04-08 00:58:10 +0700

    menambahkan class DataMahassiwa103022300047

---
+++ DataMahasiswa103022300047.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa103022300047
+{
+	public class Mahasiswa
+	{
+		public string nim { get; set; }
+		public string nama { get; set; } 
+		public string fakultas { get; set; }
+    }
+
+	public void ReadJSON()
+	{
+		string jsonString = File.ReadAllText("tp7_1_103022300047.json");
+		Mahasiswa mahasiswa = JsonSerializer.Deserialize<mahasiswa>(jsonString);	
+		Console.WriteLine($"Nama {mhs.nama} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+    }
+}
+
