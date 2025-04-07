d439476941208180262edeeb9dfc07f31ea62fff
greengoblin8 <134504798+greengoblin8@users.noreply.github.com>
2025-04-07 22:00:22 +0700

    menambahkan class DataMahasiswa103022300015

---
+++ DataMahasiswa103022300015/DataMahasiswa103022300015/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+    public class Datamahasiswa {
+    public class Mahasiswa()
+    { 
+
+    public string nama {  get; set; }
+    public string nim { get; set; }
+    public string fakultas { get; set; }
+    }
+
+    public void ReadJSON() 
+    {
+        string filePath = "tp7_1_103022300015.json";
+        string json = File.ReadAllText(filePath);
+
+        Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(json);
+
+        Console.WriteLine($"Nama {mhs.nama} nim {mhs.nim} fakultas {mhs.fakultas}");
+    }
+}
